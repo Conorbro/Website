b86727d86b8b8cb5296ce6478c75beb8beafa3b8
Conorbro <cjayjayb@gmail.com>
2015-07-08 16:31:22 +0100

    4th Commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var main = function() {
       $(".dropdown-menu").toggle();
   });
 
+  $(".photo-container").click(function() {
+    $(".photo-container").fadeIn(400);
+  });
+
 };
 
 $(document).ready(main);
